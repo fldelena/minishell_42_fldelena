eb24e00a4c815d56748ff38f89bbae0e66800bc0
Godiva Venonat <gvenonat@am-a3.msk.21-school.ru>
2021-07-26 15:41:12 +0300

    норма

--- srcs/free.c
+++ srcs/free.c
@@ -1,27 +1,36 @@
 #include "../includes/minishell.h"
 
-void free_arr(char **arr)
+void	free_arr(char **arr)
 {
-	int i;
-	i = -1;
+	int	i;
 
+	i = -1;
 	while (arr[++i])
 		free(arr[i]);
 	free(arr);
 }
 
-void free_main(char **m_str, char **arguments, t_all *all)
+void	ft_all_status(t_all *all)
 {
-	int i;
-	(void)all;
-	t_lst_pipe *tmp;
+	all->pipe = malloc(sizeof(t_lst_pipe));
+	all->pipe->next = NULL;
+	all->pipe->command = NULL;
+	all->pipe->prev = NULL;
+	all->pipe->num = -1;
+	all->pipe->count_red_pip = -1;
+}
+
+void	free_main(char **m_str, char **arguments, t_all *all)
+{
+	int			i;
+	t_lst_pipe	*tmp;
 
 	while (all->pipe->next)
 	{
 		i = 0;
 		tmp = all->pipe;
 		all->pipe = all->pipe->next;
-		while(tmp->command && tmp->command[i])
+		while (tmp->command && tmp->command[i])
 		{
 			free(tmp->command[i]);
 			i++;
@@ -30,23 +39,10 @@ void free_main(char **m_str, char **arguments, t_all *all)
 		free(tmp);
 	}
 	free(all->pipe);
-	all->pipe = malloc(sizeof(t_lst_pipe));
-	all->pipe->next = NULL;
-	all->pipe->command = NULL;
-	all->pipe->prev = NULL;
-	all->pipe->num = -1;
-	all->pipe->count_red_pip = -1;
-	i = 0;
+	ft_all_status(all);
 	if (m_str[0] != NULL)
 	{
 		free(m_str[0]);
 		(void)arguments;
-		// while (arguments != NULL && arguments[i] != NULL)
-		// {
-		// 	free(arguments[i]);
-		// 	i++;
-		// }
-		// if (arguments != NULL)
-		// 	free(arguments);
 	}
 }
